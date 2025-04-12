015f8304fc5379144427a4dc579b0e2a36a19b7b
wantingwang-27 <e0389294@u.nus.edu>
2025-04-12 23:43:42 +0800

    Update app.py
    editing the model to A2 ATE

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Training data
-X = np.array([[0], [1], [2], [3], [4]])
-y = np.array([0, 2, 4, 6, 8])
-model = LinearRegression().fit(X, y)
+# Data from the table
+data = {
+    'Y_obs': [137, 118, 124, 124, 120, 129, 122, 142, 128, 114, 132, 130, 130, 112, 132, 117, 134, 132, 121, 128],
+    'W': [0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1],
+    'X': [19.8, 23.4, 27.7, 24.6, 21.5, 25.1, 22.4, 29.3, 20.8, 20.2, 27.3, 24.5, 22.9, 18.4, 24.2, 21.0, 25.9, 23.2, 21.6, 22.8]
+}
+
+df = pd.DataFrame(data)
+
+# Fit linear regression
+model = smf.ols('Y_obs ~ W + X', data=df).fit()
 
 @app.route("/predict")
 def predict():
